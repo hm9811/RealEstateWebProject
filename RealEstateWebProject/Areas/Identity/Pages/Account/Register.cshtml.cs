563f8f4209d516d018474f6760316e5f8aa01290
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2021-07-23 08:12:57 -0700

    Added customer role when the account is being created (#9)
    * Created Identity Helper model, and created default admin
    
    * Added customer role when the account is added to server

--- RealEstateWebProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RealEstateWebProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,12 +13,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using RealEstateWebProject.Models;
 
 namespace RealEstateWebProject.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
+        private const string UserEmail = "@jayrealestate.com";
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
@@ -79,6 +81,16 @@ namespace RealEstateWebProject.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    if (user.Email.ToLower().EndsWith(UserEmail))
+                    {
+                        var roleResult = await _userManager.AddToRoleAsync(user, IdentityHelper.Customer);
+
+                        if (!roleResult.Succeeded)
+                        {
+                            _logger.LogInformation($"{user.UserName} didn't added User role");
+                        }
+                    }
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
