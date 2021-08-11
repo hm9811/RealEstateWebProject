db3ee2644c503e51bc840203426b496e2f8d5910
hm9811 <hm981198@gmail.com>
2021-08-11 15:28:36 -0700

    Added comment model and migration

---
+++ RealEstateWebProject/Data/Migrations/20210811222813_comment.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealEstateWebProject.Migrations
+{
+    public partial class comment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommentModel",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: false),
+                    comment = table.Column<string>(nullable: false),
+                    propname = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommentModel", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommentModel");
+        }
+    }
+}
