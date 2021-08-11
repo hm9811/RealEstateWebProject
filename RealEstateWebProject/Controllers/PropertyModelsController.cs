fb4c9fd02fed175c842ee4e28428914773a392b6
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2021-08-11 05:33:47 -0700

    Issue#13 property (#14)
    * added property and also added migration for it.
    
    * Added Property model, index page

---
+++ RealEstateWebProject/Controllers/PropertyModelsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealEstateWebProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateWebProject.Controllers
+{
+    public class PropertyModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PropertyModelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: PropertyModels
+        public async Task<IActionResult> Index(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            var props = from c in _context.PropertyModel select c;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                props = props.Where(x => x.name.Contains(searchString) || x.address.Contains(searchString));
+                ViewBag.visible = true;
+            }
+            else
+            {
+                ViewBag.visible = false;
+            }
+            return View(await props.AsNoTracking().ToListAsync());
+        }
+    }
+
+
+}
