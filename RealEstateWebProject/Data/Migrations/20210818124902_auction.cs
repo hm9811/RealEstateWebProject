c94dcf69285eb0dd90c838382a4c2c530032f745
hm9811 <hm981198@gmail.com>
2021-08-18 05:50:38 -0700

    added auction model that can keep track of item's auction status

---
+++ RealEstateWebProject/Data/Migrations/20210818124902_auction.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealEstateWebProject.Migrations
+{
+    public partial class auction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuctionModel",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    propname = table.Column<string>(nullable: false),
+                    custname = table.Column<string>(nullable: false),
+                    bid = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuctionModel", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuctionModel");
+        }
+    }
+}
