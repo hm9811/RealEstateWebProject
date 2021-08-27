480337f778cca4cd85411f3a638a7aa533c59fb1
hm9811 <hm981198@gmail.com>
2021-08-27 04:15:31 -0700

    add default role as customer when the account made

--- RealEstateWebProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RealEstateWebProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -80,6 +80,7 @@ namespace RealEstateWebProject.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, "Customer").Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     if (user.Email.ToLower().EndsWith(UserEmail))
