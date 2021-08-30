7856a0b0aa37d78129acd0d104bd3f15549271f0
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2021-08-30 05:02:14 -0700

    Added all the missing part for the sell part. (#31)

--- RealEstateWebProject/Controllers/PropertyController.cs
+++ RealEstateWebProject/Controllers/PropertyController.cs
@@ -188,5 +188,133 @@ namespace RealEstateWebProject.Controllers
             await _userManager.UpdateAsync(user);
             return View();
         }
+        public IActionResult Sell(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            List<SellPropertyModel> properties = new List<SellPropertyModel>();
+            var model = _context.PropertyModel.Where(x => x.propertyDetail == PropertyDetail.Unsold && x.propertyMode == PropertyMode.Sell && x.propertyStatus == PropertyStatus.Verified).ToList();
+            foreach (var m in model)
+            {
+                var sp = _context.SellPropertyModel.FirstOrDefault(x => x.name == m.name);
+                SellPropertyModel s = new SellPropertyModel
+                {
+                    id = sp.id,
+                    address = m.address,
+                    description = m.description,
+                    image = m.image,
+                    name = m.name,
+                    owner = m.owner,
+                    propertyType = m.propertyType,
+                    startprice = m.price
+                };
+                properties.Add(s);
+            }
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                properties = properties.Where(x => x.name.Contains(searchString) || x.address.Contains(searchString)).ToList();
+                ViewBag.visible = true;
+            }
+            else
+            {
+                ViewBag.visible = false;
+            }
+            return View(properties);
+        }
+        public async Task<IActionResult> SComments(int id, string comment)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                string name = await _userManager.GetUserNameAsync(user);
+                var p = _context.SellPropertyModel.FirstOrDefault(x => x.id == id);
+                CommentModel commentModel = new CommentModel
+                {
+                    comment = comment,
+                    name = name,
+                    propname = p.name
+                };
+                _context.CommentModel.Add(commentModel);
+            }
+            await _context.SaveChangesAsync();
+            return Redirect("SellDetails/" + id.ToString());
+        }
+        public async Task<IActionResult> SellDetails(int? id)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            string name = await _userManager.GetUserNameAsync(user);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sellPropertyModel = await _context.SellPropertyModel
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (sellPropertyModel == null)
+            {
+                return NotFound();
+            }
+            ViewBag.path = sellPropertyModel.image;
+            ViewBag.id = sellPropertyModel.id;
+            var comments = _context.CommentModel.Where(x => x.propname == sellPropertyModel.name).ToList();
+            var bids = _context.BidModel.Where(x => x.propname == sellPropertyModel.name && x.customername == name).ToList();
+            var p = _context.PropertyModel.FirstOrDefault(x => x.name == sellPropertyModel.name);
+            dynamic model = new ExpandoObject();
+            model.property = sellPropertyModel;
+            model.comments = comments;
+            if (comments.Count > 0)
+            {
+                ViewBag.show = true;
+            }
+            else
+            {
+                ViewBag.show = false;
+            }
+            if (bids.Count > 0)
+            {
+                model.bids = bids.ElementAt(0);
+                ViewBag.isApplied = true;
+                var b = bids.ElementAt(0);
+                var a = _context.AuctionModel.FirstOrDefault(x => x.bid == b.id);
+                if (a != null && a.custname == name && a.propname == sellPropertyModel.name)
+                {
+                    ViewBag.won = true;
+                }
+                else
+                {
+                    ViewBag.won = false;
+                }
+            }
+            else
+            {
+                ViewBag.isApplied = false;
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Bid(int? id, int bid)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            string name = await _userManager.GetUserNameAsync(user);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sellPropertyModel = await _context.SellPropertyModel
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (sellPropertyModel == null)
+            {
+                return NotFound();
+            }
+
+            BidModel b = new BidModel
+            {
+                bid = bid,
+                customername = name,
+                propname = sellPropertyModel.name
+            };
+            _context.BidModel.Add(b);
+            _context.SaveChanges();
+            return Redirect("~/Property/SellDetails/" + id.ToString());
+        }
     }
 }
